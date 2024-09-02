e6380e2cdd737effefaf9e63902a55ea3cd31764
Kartik Saxena <karrtiksaxena@gmail.com>
2024-09-02 16:05:55 +0530

    added new

---
+++ components/shadui/new.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, View ,Text} from 'react-native';
+
+const New = () => {
+    return (
+        <View>
+            <Text>
+                New
+            </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default New;
